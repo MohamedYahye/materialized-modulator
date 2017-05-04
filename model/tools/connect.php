a206aff5e0d3f10b374ae884168d1186069e8dbe
MohamedYahye <alimoha72@gmail.com>
2017-05-04 14:36:56 +0200

    Update connect.php

--- model/tools/connect.php
+++ model/tools/connect.php
@@ -12,9 +12,9 @@
 
 		function __construct() {
 			$this->conn = null;
-			$this->username = "mo_po_nl_modulat";
-			$this->password = "Rmi3mrkJT2KQ";
-			$this->server = "mo-portfolio.nl";
+			$this->username = "";
+			$this->password = "";
+			$this->server = "mo-.nl";
 
 			$this->connect();
 			
@@ -24,7 +24,7 @@
 		public function connect(){
 
 			try{
-				$this->conn = new PDO('mysql:host=95.170.72.98;dbname=mo_portfolio_nl_modulat', $this->username, $this->password);
+				$this->conn = new PDO('mysqldbname=', $this->username, $this->password);
 				$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			}catch(PDOException $e) {
 			    echo 'ERROR: ' . $e->getMessage();
